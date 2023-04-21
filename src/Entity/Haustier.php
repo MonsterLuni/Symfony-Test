f74e84d7bd2e6c29e54d962e23917a95afdef5a4
MonsterLuni <monsterluni@gmail.com>
2023-04-21 07:40:00 +0200

    END-DAY-1

---
+++ src/Entity/Haustier.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\HaustierRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: HaustierRepository::class)]
+class Haustier
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Name = null;
+
+    #[ORM\Column]
+    private ?float $Gewicht = null;
+
+    #[ORM\Column]
+    private ?float $Grösse = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $Geburtsdatum = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getGewicht(): ?float
+    {
+        return $this->Gewicht;
+    }
+
+    public function setGewicht(float $Gewicht): self
+    {
+        $this->Gewicht = $Gewicht;
+
+        return $this;
+    }
+
+    public function getGrösse(): ?float
+    {
+        return $this->Grösse;
+    }
+
+    public function setGrösse(float $Grösse): self
+    {
+        $this->Grösse = $Grösse;
+
+        return $this;
+    }
+
+    public function getGeburtsdatum(): ?\DateTimeInterface
+    {
+        return $this->Geburtsdatum;
+    }
+
+    public function setGeburtsdatum(\DateTimeInterface $Geburtsdatum): self
+    {
+        $this->Geburtsdatum = $Geburtsdatum;
+
+        return $this;
+    }
+}
